27c11f511625ab2f6d3e59e8a0c888900b46c16e
Javier Diaz <diazjavierdavid@hotmail.com>
2021-10-17 11:53:45 -0300

    Se crean las paginas de cada post a partir del id recibido en url

--- src/app/components/post-detail/post-detail.component.ts
+++ src/app/components/post-detail/post-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { JsonPostsService } from 'src/app/services/json-posts.service';
 
 @Component({
   selector: 'app-post-detail',
@@ -7,9 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostDetailComponent implements OnInit {
 
-  constructor() { }
+  post: any = {}
+
+  constructor(private router: ActivatedRoute,
+    private postService: JsonPostsService) {
+
+      this.router.params.subscribe( params => {
+        this.getPost(params['id']);
+      })
+     }
 
   ngOnInit(): void {
+  }
+
+  getPost(id: number) {
+
+    this.postService.getPostById(id)
+        .subscribe(data => {
+          this.post = data;
+        })
   }
 
 }
