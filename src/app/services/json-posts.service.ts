0a5df6a912c882cca1249e6c67fa04707be03283
Javier Diaz <diazjavierdavid@hotmail.com>
2021-10-17 10:58:42 -0300

    Se crea el servicio de JSONplaceholder

---
+++ src/app/services/json-posts.service.ts
@@ -0,0 +1,35 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JsonPostsService {
+
+  constructor(private http: HttpClient) { }
+
+  getQuery(query:string){
+    const url = `https://jsonplaceholder.typicode.com/${query}`;
+
+    return this.http.get(url);
+  }
+
+  getPosts(){
+    return this.getQuery('posts');
+  }
+
+  getPostById(id:string){
+    const postQuery = `posts/${id}`;
+
+    return this.getQuery(postQuery);
+  }
+
+  getPostComments(id: string){
+    const commentsQuery = `posts/${id}/comments`;
+    
+    return this.getQuery(commentsQuery);
+  }
+
+
+}
